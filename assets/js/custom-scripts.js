fe536156485062b60b05a9f16fad50ceafdec562
Bob Zheng <leavenotrace@foxmail.com>
2016-04-17 07:11:05 +0800

    add dream

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -21,244 +21,167 @@
                 }
             });
 
- 
-            var data_info;
-            $.ajax({
-                type: 'get',
-                url: 'http://getazlnx001.chinacloudapp.cn:8080/emstracking?method=lastdaychart&item_no=01',
-                dataType: 'jsonp',
-                success: function (data) {
-                    data_info = data;
-
-                    Morris.Line({
-                        element: 'Dust_1_chart',
-                        data: data_info,
-                        xkey: 'measuredtime',
-                        ykeys: ['measured_value', 'max_threshold_value'],
-                        labels: ['Value', 'Threshold'],
-                        hideHover: 'auto',
-                        resize: true
-                    });
-
-                }
+            /* MORRIS BAR CHART
+			-----------------------------------------*/
+            Morris.Bar({
+                element: 'morris-bar-chart',
+                data: [{
+                    y: '2006',
+                    a: 100,
+                    b: 90
+                }, {
+                    y: '2007',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2008',
+                    a: 50,
+                    b: 40
+                }, {
+                    y: '2009',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2010',
+                    a: 50,
+                    b: 40
+                }, {
+                    y: '2011',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2012',
+                    a: 100,
+                    b: 90
+                }],
+                xkey: 'y',
+                ykeys: ['a', 'b'],
+                labels: ['Series A', 'Series B'],
+                hideHover: 'auto',
+                resize: true
             });
 
-            var data_info;
-            $.ajax({
-                type: 'get',
-                url: 'http://getazlnx001.chinacloudapp.cn:8080/emstracking?method=lastdaychart&item_no=02',
-                dataType: 'jsonp',
-                success: function (data) {
-                    data_info = data;
-
-                    Morris.Line({
-                        element: 'SQ2_1_chart',
-                        data: data_info,
-                        xkey: 'measuredtime',
-                        ykeys: ['measured_value', 'max_threshold_value'],
-                        labels: ['Value', 'Threshold'],
-                        hideHover: 'auto',
-                        resize: true
-                    });
-
-                }
+            /* MORRIS DONUT CHART
+			----------------------------------------*/
+            Morris.Donut({
+                element: 'morris-donut-chart',
+                data: [{
+                    label: "Download Sales",
+                    value: 12
+                }, {
+                    label: "In-Store Sales",
+                    value: 30
+                }, {
+                    label: "Mail-Order Sales",
+                    value: 20
+                }],
+                resize: true
             });
 
-            var data_info;
-            $.ajax({
-                type: 'get',
-                url: 'http://getazlnx001.chinacloudapp.cn:8080/emstracking?method=lastdaychart&item_no=03',
-                dataType: 'jsonp',
-                success: function (data) {
-                    data_info = data;
+            /* MORRIS AREA CHART
+			----------------------------------------*/
 
-                    Morris.Line({
-                        element: 'NO_1_chart',
-                        data: data_info,
-                        xkey: 'measuredtime',
-                        ykeys: ['measured_value', 'max_threshold_value'],
-                        labels: ['Value', 'Threshold'],
-                        hideHover: 'auto',
-                        resize: true
-                    });
-
-                }
+            Morris.Area({
+                element: 'morris-area-chart',
+                data: [{
+                    period: '2010 Q1',
+                    iphone: 2666,
+                    ipad: null,
+                    itouch: 2647
+                }, {
+                    period: '2010 Q2',
+                    iphone: 2778,
+                    ipad: 2294,
+                    itouch: 2441
+                }, {
+                    period: '2010 Q3',
+                    iphone: 4912,
+                    ipad: 1969,
+                    itouch: 2501
+                }, {
+                    period: '2010 Q4',
+                    iphone: 3767,
+                    ipad: 3597,
+                    itouch: 5689
+                }, {
+                    period: '2011 Q1',
+                    iphone: 6810,
+                    ipad: 1914,
+                    itouch: 2293
+                }, {
+                    period: '2011 Q2',
+                    iphone: 5670,
+                    ipad: 4293,
+                    itouch: 1881
+                }, {
+                    period: '2011 Q3',
+                    iphone: 4820,
+                    ipad: 3795,
+                    itouch: 1588
+                }, {
+                    period: '2011 Q4',
+                    iphone: 15073,
+                    ipad: 5967,
+                    itouch: 5175
+                }, {
+                    period: '2012 Q1',
+                    iphone: 10687,
+                    ipad: 4460,
+                    itouch: 2028
+                }, {
+                    period: '2012 Q2',
+                    iphone: 8432,
+                    ipad: 5713,
+                    itouch: 1791
+                }],
+                xkey: 'period',
+                ykeys: ['iphone', 'ipad', 'itouch'],
+                labels: ['iPhone', 'iPad', 'iPod Touch'],
+                pointSize: 2,
+                hideHover: 'auto',
+                resize: true
             });
 
-
-
-            var data_info;
-            $.ajax({
-                type: 'get',
-                url: 'http://getazlnx001.chinacloudapp.cn:8080/emstracking?method=lastdaychart&item_no=04',
-                dataType: 'jsonp',
-                success: function (data) {
-                    data_info = data;
-
-                    Morris.Line({
-                        element: 'Dust_2_chart',
-                        data: data_info,
-                        xkey: 'measuredtime',
-                        ykeys: ['measured_value', 'max_threshold_value'],
-                        labels: ['Value', 'Threshold'],
-                        hideHover: 'auto',
-                        resize: true
-                    });
-
-                }
+            /* MORRIS LINE CHART
+			----------------------------------------*/
+            Morris.Line({
+                element: 'morris-line-chart',
+                data: [{
+                    y: '2006',
+                    a: 100,
+                    b: 90
+                }, {
+                    y: '2007',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2008',
+                    a: 50,
+                    b: 40
+                }, {
+                    y: '2009',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2010',
+                    a: 50,
+                    b: 40
+                }, {
+                    y: '2011',
+                    a: 75,
+                    b: 65
+                }, {
+                    y: '2012',
+                    a: 100,
+                    b: 90
+                }],
+                xkey: 'y',
+                ykeys: ['a', 'b'],
+                labels: ['Series A', 'Series B'],
+                hideHover: 'auto',
+                resize: true
             });
-
-            var data_info;
-            $.ajax({
-                type: 'get',
-                url: 'http://getazlnx001.chinacloudapp.cn:8080/emstracking?method=lastdaychart&item_no=05',
-                dataType: 'jsonp',
-                success: function (data) {
-                    data_info = data;
-
-                    Morris.Line({
-                        element: 'SQ2_2_chart',
-                        data: data_info,
-                        xkey: 'measuredtime',
-                        ykeys: ['measured_value', 'max_threshold_value'],
-                        labels: ['Value', 'Threshold'],
-                        hideHover: 'auto',
-                        resize: true
-                    });
-
-                }
-            });
-
-            var data_info;
-            $.ajax({
-                type: 'get',
-                url: 'http://getazlnx001.chinacloudapp.cn:8080/emstracking?method=lastdaychart&item_no=06',
-                dataType: 'jsonp',
-                success: function (data) {
-                    data_info = data;
-
-                    Morris.Line({
-                        element: 'NO_2_chart',
-                        data: data_info,
-                        xkey: 'measuredtime',
-                        ykeys: ['measured_value', 'max_threshold_value'],
-                        labels: ['Value', 'Threshold'],
-                        hideHover: 'auto',
-                        resize: true
-                    });
-
-                }
-            });
-
-
-            var data_info;
-            $.ajax({
-                type: 'get',
-                url: 'http://getazlnx001.chinacloudapp.cn:8080/emstracking?method=lastdaychart&item_no=07',
-                dataType: 'jsonp',
-                success: function (data) {
-                    data_info = data;
-
-                    Morris.Line({
-                        element: 'Dust_3_chart',
-                        data: data_info,
-                        xkey: 'measuredtime',
-                        ykeys: ['measured_value', 'max_threshold_value'],
-                        labels: ['Value', 'Threshold'],
-                        hideHover: 'auto',
-                        resize: true
-                    });
-
-                }
-            });
-
-            var data_info;
-            $.ajax({
-                type: 'get',
-                url: 'http://getazlnx001.chinacloudapp.cn:8080/emstracking?method=lastdaychart&item_no=08',
-                dataType: 'jsonp',
-                success: function (data) {
-                    data_info = data;
-
-                    Morris.Line({
-                        element: 'SQ2_3_chart',
-                        data: data_info,
-                        xkey: 'measuredtime',
-                        ykeys: ['measured_value', 'max_threshold_value'],
-                        labels: ['Value', 'Threshold'],
-                        hideHover: 'auto',
-                        resize: true
-                    });
-
-                }
-            });
-
-            var data_info;
-            $.ajax({
-                type: 'get',
-                url: 'http://getazlnx001.chinacloudapp.cn:8080/emstracking?method=lastdaychart&item_no=09',
-                dataType: 'jsonp',
-                success: function (data) {
-                    data_info = data;
-
-                    Morris.Line({
-                        element: 'NO_3_chart',
-                        data: data_info,
-                        xkey: 'measuredtime',
-                        ykeys: ['measured_value', 'max_threshold_value'],
-                        labels: ['Value', 'Threshold'],
-                        hideHover: 'auto',
-                        resize: true
-                    });
-
-                }
-            });
-
-            var data_info;
-            $.ajax({
-                type: 'get',
-                url: 'http://getazlnx001.chinacloudapp.cn:8080/emstracking?method=lastdaychart&item_no=10',
-                dataType: 'jsonp',
-                success: function (data) {
-                    data_info = data;
-
-                    Morris.Line({
-                        element: 'COD_chart',
-                        data: data_info,
-                        xkey: 'measuredtime',
-                        ykeys: ['measured_value', 'max_threshold_value'],
-                        labels: ['Value', 'Threshold'],
-                        hideHover: 'auto',
-                        resize: true
-                    });
-
-                }
-            });
-
-            var data_info;
-            $.ajax({
-                type: 'get',
-                url: 'http://getazlnx001.chinacloudapp.cn:8080/emstracking?method=lastdaychart&item_no=11',
-                dataType: 'jsonp',
-                success: function (data) {
-                    data_info = data;
-
-                    Morris.Line({
-                        element: 'NH_chart',
-                        data: data_info,
-                        xkey: 'measuredtime',
-                        ykeys: ['measured_value', 'max_threshold_value'],
-                        labels: ['Value', 'Threshold'],
-                        hideHover: 'auto',
-                        resize: true
-                    });
-
-                }
-            });
-
-
-
-
+           
      
         },
 
